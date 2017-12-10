9a5c29c696f16da050e46b19b5f64ceb1febecdd
Lydiasama <hallelujah.lydia@gmail.com>
2017-12-10 22:06:32 +0700

    init home activity

--- app/src/main/java/com/worship/the/lord/views/activity/HomeActivity.kt
+++ app/src/main/java/com/worship/the/lord/views/activity/HomeActivity.kt
@@ -1,12 +1,15 @@
-package com.worship.the.lord
+package com.worship.the.lord.views.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.worship.the.lord.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
     }
 }
